97be138267ba2e067c6c57470149218ef2435ba2
MikeMcGregorTrios <42556433+MikeMcGregorTrios@users.noreply.github.com>
2018-11-27 17:22:11 -0500

    Initial Commit

---
+++ MageRoyale/Assets/MagicMissleBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicMissleBehaviour : MonoBehaviour {
+
+    public float damage = 10.0f;
+
+
+    void Start()
+    {
+        var tm = GetComponentInChildren<RFX4_TransformMotion>(true);
+        if (tm != null) tm.CollisionEnter += Tm_CollisionEnter;
+    }
+
+    private void Tm_CollisionEnter(object sender, RFX4_TransformMotion.RFX4_CollisionInfo e)
+    {
+        Debug.Log(e.Hit.transform.name); //will print collided object name to the console.
+        if(e.Hit.transform.tag == "Player")
+        {
+            e.Hit.transform.GetComponent<PlayerController>().TakeDamage(damage);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
